e3ad93732c08702539dfc1f5751e31066e14f98e
Jayant Kurve <jayantkurve9@gmail.com>
2024-02-19 23:04:23 +0530

    Basic working structure of calculator.

---
+++ script.js
@@ -0,0 +1,17 @@
+let string = "";
+const buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach(button => {
+	button.addEventListener('click', (e)=> {
+		if (e.target.innerHTML == '=') {
+			string = eval(string);
+		} else if (e.target.innerHTML == 'C') {
+			string = '';
+		} else {
+			console.log(e.target);
+			string = string + e.target.innerHTML;
+		}
+		
+		document.querySelector('input').value = string;
+	});
+});
